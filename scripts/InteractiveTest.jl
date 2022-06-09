862b5a5f3e3cf409930be452172355edb12f3aaa
Daniel <dan.steinmaurer@gmail.com>
2022-06-10 00:06:08 +0200

    extract functions from mainloop

--- scripts/InteractiveTest.jl
+++ scripts/InteractiveTest.jl
@@ -19,70 +19,43 @@ function mainloop()
 	displayinteractive(state)
 	while true
 		t = readline()
-
-		if(startswith(t, "exit"))
-			@info "Shutting down"
-			break
-		end
-
-		args = split(t, " ")
-
-		if(startswith(t, "re-plot"))
-			displayinteractive(state)
-			continue
-		end
-
-		if(startswith(t, "plot"))
-			if length(args) < 2
-				step_size = 0.005
-
-				x_range_tmp = -2:step_size:2
-				y_range_tmp = -2:step_size:2
-
-				state = InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
-			else
-				x_range_args = split(args[2], ":")
-				y_range_args = split(args[3], ":")
-
-				x_range = parse(Float64, x_range_args[1]) : step_size : parse(Float64, x_range_args[2])
-				y_range = parse(Float64, y_range_args[1]) : step_size : parse(Float64, y_range_args[2])
-
-				state = InteractiveState(x_range, y_range, state.seriesfun)
-			end
-			
-			displayinteractive(state)
-			continue
-		end
-
-		if(startswith(t, "save"))
-			savecurrentdata(state, args)
-			continue
-		end
-
-		if startswith(t, "set")
-			if(startswith(args[2], "res"))
-				step_size = parse(Float64 , args[3])
-			
-				x_range_tmp = state.x_range[1]:step_size:state.x_range[end]
-				y_range_tmp = state.y_range[1]:step_size:state.y_range[end]
-
-				state = InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
+		try
+			if(startswith(t, "exit"))
+				@info "Shutting down"
+				break
 			end
 
-			if(startswith(args[2], "mode"))
-				if startswith(args[3], "mb")
-					state = InteractiveState(state.x_range, state.y_range, Mandelbrot.mandelbrot_series_bound)
+			args = split(t, " ")
 
-				elseif startswith(args[3], "jl")
-					c = parse(ComplexF64, args[4])
-					state = InteractiveState(state.x_range, state.y_range, (z0, p_x_range, p_y_range) -> Mandelbrot.julia_series_bound(c, z0, p_x_range, p_y_range))
+			if(startswith(t, "re-plot"))
+				displayinteractive(state)
+				continue
+			end
+
+			if(startswith(t, "plot"))
+				if length(args) < 2
+					state = defaultranges(state)
+				else
+					state = parseranges(state, args)
 				end
-			end
 			
-			displayinteractive(state)
-			continue
-		end
+				displayinteractive(state)
+				continue
+			end
 
+			if(startswith(t, "save"))
+				savecurrentdata(state, args)
+				continue
+			end
+
+			if startswith(t, "set")
+				state = setswitch(state, args)
+				displayinteractive(state)
+				continue
+			end
+		catch e
+			@error sprint(showerror, e)
+		end
 	end
 end
 
@@ -132,7 +105,66 @@ function calculatedata(state)
 	return n_till_escape
 end
 
-mutable struct InteractiveState
+function defaultranges(state)
+	step_size = 0.005
+
+	x_range_tmp = -2:step_size:2
+	y_range_tmp = -2:step_size:2
+
+	return InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
+end
+
+function parseranges(state, args)
+	x_range_args = split(args[2], ":")
+	y_range_args = split(args[3], ":")
+
+	step_size = state.x_range[2] - state.x_range[1]
+
+	x_range = parse(Float64, x_range_args[1]) : step_size : parse(Float64, x_range_args[2])
+	y_range = parse(Float64, y_range_args[1]) : step_size : parse(Float64, y_range_args[2])
+
+	return InteractiveState(x_range, y_range, state.seriesfun)
+end
+
+function setswitch(state, args)
+	if(startswith(args[2], "res"))
+		return setres(state, args)
+	end
+
+	if(startswith(args[2], "mode"))
+		return setmode(state, args)
+	end
+	
+	return state
+end
+
+function setres(state, args)
+	step_size = parse(Float64 , args[3])
+			
+	x_range_tmp = state.x_range[1]:step_size:state.x_range[end]
+	y_range_tmp = state.y_range[1]:step_size:state.y_range[end]
+
+	return InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
+end
+
+function setmode(state, args)
+	if startswith(args[3], "mb")
+		return InteractiveState(state.x_range, state.y_range, Mandelbrot.mandelbrot_series_bound)
+
+	elseif startswith(args[3], "jl")
+		c = parse(ComplexF64, args[4])
+		return InteractiveState(
+			state.x_range,
+			state.y_range,
+			(z0, p_x_range, p_y_range) -> Mandelbrot.julia_series_bound(c, z0, p_x_range, p_y_range)
+		)
+	end
+
+	@warn "$(args[2]) not a valid mode..."
+	return state
+end
+
+struct InteractiveState
 	x_range::StepRangeLen
 	y_range::StepRangeLen
 	seriesfun::Function
