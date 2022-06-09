122c19a6a9ae1a2cd166801dda9171c3b8ddb405
Daniel <dan.steinmaurer@gmail.com>
2022-06-09 23:33:35 +0200

    add State object

--- scripts/InteractiveTest.jl
+++ scripts/InteractiveTest.jl
@@ -6,28 +6,29 @@ using Logging
 include(srcdir() * "\\Mandelbrot.jl")
 
 
-function main_loop()
+function mainloop()
 	t = ""
+	
 	step_size = 0.005
-	x_range = -2:step_size:2
-	y_range = -2:step_size:2
+	x_range_init = -2:step_size:2
+	y_range_init = -2:step_size:2
+	f = Mandelbrot.mandelbrot_series_bound
 
-	series_fun = Mandelbrot.mandelbrot_series_bound
+	state = InteractiveState(x_range_init, y_range_init, f)
 
-	display_ia(series_fun, x_range, y_range)
+	displayinteractive(state)
 	while true
 		t = readline()
 
 		if(startswith(t, "exit"))
-			@warn "Shutting down"
+			@info "Shutting down"
 			break
 		end
 
 		args = split(t, " ")
-		#println(args)
 
 		if(startswith(t, "re-plot"))
-			display_ia(series_fun, x_range, y_range)
+			displayinteractive(state)
 			continue
 		end
 
@@ -35,22 +36,26 @@ function main_loop()
 			if length(args) < 2
 				step_size = 0.005
 
-				x_range = -2:step_size:2
-				y_range = -2:step_size:2
+				x_range_tmp = -2:step_size:2
+				y_range_tmp = -2:step_size:2
+
+				state = InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
 			else
 				x_range_args = split(args[2], ":")
 				y_range_args = split(args[3], ":")
 
 				x_range = parse(Float64, x_range_args[1]) : step_size : parse(Float64, x_range_args[2])
 				y_range = parse(Float64, y_range_args[1]) : step_size : parse(Float64, y_range_args[2])
+
+				state = InteractiveState(x_range, y_range, state.seriesfun)
 			end
 			
-			display_ia(series_fun, x_range, y_range)
+			displayinteractive(state)
 			continue
 		end
 
 		if(startswith(t, "save"))
-			save_current_data(series_fun, args, x_range, y_range)
+			savecurrentdata(state, args)
 			continue
 		end
 
@@ -58,68 +63,67 @@ function main_loop()
 			if(startswith(args[2], "res"))
 				step_size = parse(Float64 , args[3])
 			
-				x_range_tmp = x_range[1]:step_size:x_range[end]
-				y_range_tmp = y_range[1]:step_size:y_range[end]
+				x_range_tmp = state.x_range[1]:step_size:state.x_range[end]
+				y_range_tmp = state.y_range[1]:step_size:state.y_range[end]
 
-				x_range = x_range_tmp
-				y_range = y_range_tmp
+				state = InteractiveState(x_range_tmp, y_range_tmp, state.seriesfun)
 			end
 
 			if(startswith(args[2], "mode"))
 				if startswith(args[3], "mb")
-					series_fun = Mandelbrot.mandelbrot_series_bound
+					state = InteractiveState(state.x_range, state.y_range, Mandelbrot.mandelbrot_series_bound)
 
 				elseif startswith(args[3], "jl")
 					c = parse(ComplexF64, args[4])
-					series_fun = (z0, p_x_range, p_y_range) -> Mandelbrot.julia_series_bound(c, z0, p_x_range, p_y_range)
+					state = InteractiveState(state.x_range, state.y_range, (z0, p_x_range, p_y_range) -> Mandelbrot.julia_series_bound(c, z0, p_x_range, p_y_range))
 				end
 			end
 			
-			display_ia(series_fun ,x_range, y_range)
+			displayinteractive(state)
 			continue
 		end
 
 	end
 end
 
-function save_current_data(series_fun ,args, x_range, y_range)
+function savecurrentdata(state ,args)
 	@debug args
 	@debug String(args[2])
-	save_ia(series_fun, plotsdir() * "\\" * String(args[2]), x_range, y_range)
+	saveinteractive(state, plotsdir() * "\\" * String(args[2]))
 end
 
-function display_ia(series_fun ,x_range, y_range)
-	@info "calculating manddelbrot series in range of X: $(x_range) and Y: $(y_range)"
-	n_till_escape = calc_data(series_fun, x_range, y_range)
+function displayinteractive(state)
+	@info "calculating manddelbrot series in range of X: $(state.x_range) and Y: $(state.y_range)"
+	n_till_escape = calculatedata(state)
 
 	GC.gc()
 
 	@info "displaying data in heatmap..."
-	colorData = reshape(n_till_escape, length(y_range), length(x_range))
-	plt = heatmap(x_range, y_range, colorData)
+	colorData = reshape(n_till_escape, length(state.y_range), length(state.x_range))
+	plt = heatmap(state.x_range, state.y_range, colorData)
 	display(plt)
 end
 
-function save_ia(series_fun ,filename::String ,x_range, y_range)
-	@info "calculating manddelbrot series in range of X: $(x_range) and Y: $(y_range)"
-	n_till_escape = calc_data(series_fun, x_range, y_range)
+function saveinteractive(state ,filename::String)
+	@info "calculating manddelbrot series in range of X: $(state.x_range) and Y: $(state.y_range)"
+	n_till_escape = calculatedata(state)
 
 	GC.gc()
 
 	@info "rendering data in heatmap..."
-	colorData = reshape(n_till_escape, length(y_range), length(x_range))
-	plt = heatmap(x_range, y_range, colorData)
+	colorData = reshape(n_till_escape, length(state.y_range), length(state.x_range))
+	plt = heatmap(state.x_range, state.y_range, colorData)
 
 	@info "saving fig"
 	savefig(plt, filename)
 end
 
-function calc_data(series_fun , x_range, y_range)
+function calculatedata(state)
 	n_till_escape = Array{Float32}(undef, 0)
 
-	for x in x_range
-		for y in y_range
-			escapes_after = series_fun(x + y*im, 30, 2)
+	for x in state.x_range
+		for y in state.y_range
+			escapes_after = state.seriesfun(x + y*im, 30, 2)
 
 			append!(n_till_escape, escapes_after)
 		end
@@ -128,4 +132,12 @@ function calc_data(series_fun , x_range, y_range)
 	return n_till_escape
 end
 
-main_loop()
+mutable struct InteractiveState
+	x_range::StepRangeLen
+	y_range::StepRangeLen
+	seriesfun::Function
+
+	InteractiveState(x, y , f) = new(x, y, f)
+end
+
+mainloop()
